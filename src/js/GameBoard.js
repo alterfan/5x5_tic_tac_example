3541f4c865d6a880b09ef29935c4c5e98193f97c
Vasily Printsev <alterfan@outlook.com>
2020-08-04 16:19:04 +0300

    fix bug after restart scene

--- src/js/GameBoard.js
+++ src/js/GameBoard.js
@@ -20,10 +20,10 @@ export default class GameBoard extends Phaser.Scene {
 		this.load.image('x', './assets/images/x.png')
 		this.load.image('o', './assets/images/o.png')
 		this.load.image('play_again', './assets/images/play_again.png')
-		this.graphics = this.add.graphics({ lineStyle: { width: 2, color: 0xffffff } }).setInteractive()
 	}
 	create() {
 		this.state = { scorePlayer: 0, scoreAI: 0, whoseTurn: 'x', lastUpdates: [null, null], gameEnd: false }
+		this.graphics = this.add.graphics({ lineStyle: { width: 2, color: 0xffffff } }).setInteractive()
 		this.board = new Board(this, 0, 0, this.graphics)
 		this.input.setTopOnly(false)
 		this.scale.on('orientationchange', function (orientation) {
